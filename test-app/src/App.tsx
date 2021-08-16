e36e3764fd4a0e52181100991fbc40449406917a
gojutin <gojutin@gmail.com>
2021-08-16 13:45:20 -0400

    docs: test site updates

--- test-app/src/App.tsx
+++ test-app/src/App.tsx
@@ -1,9 +1,12 @@
 // 'useState' is defined but never used.eslint@typescript-eslint/no-unused-vars
+import something from "./file";
 import React, { useState } from "react";
 
 function App() {
   // Unexpected let, use const instead.eslintfunctional/no-let
   let x = 3;
+
+  console.log(something);
 
   return (
     <div>
